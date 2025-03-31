7a87fb4bb03de886e74ab4abb4d6d1794328969c
Sambit99 <sambitkumar75@gmail.com>
2025-03-31 12:21:04 +0530

    feat: Health Endpoint Setup

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -3,6 +3,7 @@ import ApiResponse from '../util/ApiResponse';
 import { StatusCode } from '../constant/statusCodes';
 import { ResponseMessage } from '../constant/responseMessage';
 import ApiError from '../util/ApiError';
+import Quicker from '../util/Quicker';
 
 export default {
   self: (req: Request, res: Response, next: NextFunction) => {
@@ -10,6 +11,18 @@ export default {
       ApiResponse(req, res, StatusCode.OK, ResponseMessage.SUCCESS, {});
     } catch (error) {
       ApiError(next, error, req, StatusCode.INTERNAL_SERVER_ERROR);
+    }
+  },
+  health: (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const healthData = {
+        application: Quicker.getApplicationHealth(),
+        system: Quicker.getSystemHealth(),
+        timestamp: Date.now()
+      };
+      ApiResponse(req, res, StatusCode.OK, ResponseMessage.SUCCESS, healthData);
+    } catch (error) {
+      ApiError(next, error, req, StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
     }
   }
 };
