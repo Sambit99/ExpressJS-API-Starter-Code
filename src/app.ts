499ff1c496e587277a70569041e4547a7f0f626b
Sambit99 <sambitkumar75@gmail.com>
2025-03-31 11:59:57 +0530

    feat: Global error handler and 404 Handler setup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import path from 'path';
 
 const app: Application = express();
@@ -10,6 +10,22 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 import ApiRouter from './router/api.router';
+import globalErrorHandler from './middleware/globalErrorHandler';
+import { ResponseMessage } from './constant/responseMessage';
+import ApiError from './util/ApiError';
+import { StatusCode } from './constant/statusCodes';
 app.use('/api/v1', ApiRouter);
+
+// 404 Error Handler
+app.use((req: Request, _: Response, next: NextFunction) => {
+  try {
+    throw new Error(ResponseMessage.NOT_FOUND('Route'));
+  } catch (error) {
+    ApiError(next, error, req, StatusCode.NOT_FOUND);
+  }
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
