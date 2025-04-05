a5249fcdb29840d208a14cfb361f7bce0c7178e9
Sambit99 <sambitkumar75@gmail.com>
2025-04-05 13:00:21 +0530

    feat: Add ErrorObject and Quicker utility modules for error handling and system health checks

---
+++ src/util/ErrorObject.ts
@@ -0,0 +1,39 @@
+import { Request } from 'express';
+import { HttpError } from '../types/types';
+import { ApplicationEnvironment } from '../constant/application';
+import { ResponseMessage } from '../constant/responseMessage';
+import config from '../config/config';
+import Logger from './Logger';
+
+export default (
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+  err: Error | unknown,
+  req: Request,
+  errorStatusCode: number = 500,
+  customErrorMessage: string = ''
+): HttpError => {
+  const isError = err instanceof Error;
+  const errorObj: HttpError = {
+    success: false,
+    statusCode: errorStatusCode,
+    request: {
+      ip: req.ip,
+      method: req.method,
+      url: req.originalUrl
+    },
+
+    message: customErrorMessage ? customErrorMessage : isError ? err.message : ResponseMessage.INTERNAL_SERVER_ERROR,
+    data: null,
+    trace: isError ? { error: err.stack } : err instanceof Object ? err : null
+  };
+
+  if (config.ENV === ApplicationEnvironment.PRODUCTION) {
+    delete errorObj.request.ip;
+    delete errorObj.trace;
+  }
+
+  //   Log
+  Logger.info(`CONTROLLER ERROR`, { meta: errorObj });
+
+  return errorObj;
+};
